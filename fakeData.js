28a318de7fd452cb744507047380df40777c9882
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-06-15 10:02:19 -0300

    chore: add more users to fakeData

--- fakeData.js
+++ fakeData.js
@@ -4,7 +4,17 @@ const fakeData  =  [
         id: 1,
         name: "João Oliveira",
         job: "Desenvolvedor"
-    }
+    },
+    {
+        id: 2,
+        name: "Raphael Cirelli",
+        job: "Desenvolvedor"
+    },
+    {
+        id: 3,
+        name: "Raiane Santos",
+        job: "RH"
+    },
 ]
 
 
