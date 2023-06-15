9af7ba4100c8d73dc5c4c7a42fbb599c2340ac1e
Raphael Cirelli <raphael.cirelli@protonmail.com>
2023-06-15 10:56:37 -0300

    refactor: function on PUT /users endpoint

--- teste4.js
+++ teste4.js
@@ -1,13 +1,32 @@
-var data =  require("./fakeData");
+const data = require("./fakeData");
+const { BAD_REQUEST, NOT_FOUND } = require("./httpStatusCodes");
 
-module.exports =  function(req, res) {
-  
-    var id =  req.query.id;
+module.exports = function(req, res) {
+    const id = Number(req.query.id);
+    const name = req.body.name;
+    const job = req.body.job;
 
-    const reg = data.find(d => id == id);
-    reg.name = req.body.name;
-    reg.job = req.body.job;
+    if (!id) {
+        return res.status(BAD_REQUEST).send({ message: "User id must be present in query parameters" });
+    }
 
-    res.send(reg);
+    if (!name && !job) {
+        return res.status(BAD_REQUEST).send({ message: "name or job must be present in request body to be updated" });
+    }
 
+    const userIndexToUpdate = data.findIndex((user) => user.id === id);
+
+    if (userIndexToUpdate === -1) {
+        return res.status(NOT_FOUND).send({ message: "user not found" });
+    }
+
+    const userToUpdate = data[userIndexToUpdate];
+
+    data[userIndexToUpdate] = {
+        ...userToUpdate,
+        name: name || userToUpdate.name,
+        job: job || userToUpdate.job,
+    }
+
+    return res.send(data[userIndexToUpdate]);
 };
